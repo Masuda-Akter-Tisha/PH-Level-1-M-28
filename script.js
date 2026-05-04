46435eb20a6ed355464604f4dfebd9a3b3a2e905
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-04 22:53:52 +0600

    explored JSON

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// JS object
 const person = {
     name : 'halima',
     food : 'rice',
@@ -5,4 +6,15 @@ const person = {
     age: 23,
     isRich : false,
 }
-console.log(person);
+console.log(person, typeof person);
+
+// JSON ---> JS object with notation
+// JSON.stringify ---> It will convert JS object to JSON string
+// JSON.parse ---> It will convert JSON file to JS object
+
+const personJson = JSON.stringify (person);
+console.log(personJson, typeof personJson);
+
+// JSON file ---> JS object
+const personParse = JSON.parse (personJson);
+console.log(personParse);
