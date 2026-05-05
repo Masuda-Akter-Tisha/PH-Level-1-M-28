29f122d2d0e71bbecd6a44a80997de77070ff06a
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-05 23:24:07 +0600

    explored JSON and API request

--- script.js
+++ script.js
@@ -6,22 +6,32 @@ const person = {
     age: 23,
     isRich : false,
 }
-console.log(person, typeof person);
+// console.log(person, typeof person);
 
 // JSON ---> JS object with notation
 // JSON.stringify ---> It will convert JS object to JSON string
 // JSON.parse ---> It will convert JSON file to JS object
 
 const personJson = JSON.stringify (person);
-console.log(personJson, typeof personJson);
+// console.log(personJson, typeof personJson);
 
 // JSON file ---> JS object
 const personParse = JSON.parse (personJson);
-console.log(personParse);
+// console.log(personParse);
 
-
+// void function ---> function that returns nothing
   const dataLoad = () => {
-     fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then ((response) => response.json ())
+     fetch('https://jsonplaceholder.typicode.com/todos/1') // promise of response
+      .then ((res) => res.json()) // promise of json data
       .then ((data) => console.log(data))
-  }    
+  }  
+  
+  const dataPost = () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+
+    fetch (url)
+    .then (res => res.json ())
+    .then (json => console.log(json))
+  }
+  
+  
