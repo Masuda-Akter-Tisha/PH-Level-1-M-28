a964fd1d49c4e8033839fea18f3b6fac9a296f9e
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-06 10:52:07 +0600

    fetch posts from API and loop through data using forEach

--- script.js
+++ script.js
@@ -20,18 +20,27 @@ const personParse = JSON.parse (personJson);
 // console.log(personParse);
 
 // void function ---> function that returns nothing
-  const dataLoad = () => {
+// Data-Load
+  const loadData = () => {
      fetch('https://jsonplaceholder.typicode.com/todos/1') // promise of response
       .then ((res) => res.json()) // promise of json data
       .then ((data) => console.log(data))
   }  
   
-  const dataPost = () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-
-    fetch (url)
+  // Data-Post
+   const loadPost = () => {
+     const url = 'https://jsonplaceholder.typicode.com/posts';
+     fetch (url)
     .then (res => res.json ())
-    .then (json => console.log(json))
+    .then (json => {
+      console.log(json);
+      displayPost (json)
+    })
+   }
+
+  const displayPost = (posts) => {
+    posts.forEach (post => {
+      console.log(post);
+    });  
   }
-  
   
